df398d4fd69836f27b9ed3c681269455c4f1b791
y-solb <ysolb99@gmail.com>
2022-03-11 00:50:16 +0900

    Feat: Check name validation

--- src/components/molecules/InputBox.js
+++ src/components/molecules/InputBox.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const InputBox = ({ placeholder, inputTitle, warning, handleClick }) => (
-  <InputWrapper>
-    <Input warning={warning} placeholder={placeholder} onClick={handleClick} />
-    {warning && <WarningText>{inputTitle} 항목은 필수 정보입니다</WarningText>}
-  </InputWrapper>
-);
+const InputBox = ({ placeholder, inputTitle }) => {
+  const [name, setName] = useState('');
+  const [isNull, setIsNull] = useState(false);
+
+  const handleBlur = () => {
+    if (name.length === 0) {
+      setIsNull(true);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.value.length > 0) {
+      setIsNull(false);
+    } else {
+      setIsNull(true);
+    }
+    setName(e.target.value);
+  };
+
+  console.log(name);
+
+  return (
+    <InputWrapper>
+      <Input
+        warning={isNull}
+        placeholder={placeholder}
+        onBlur={handleBlur}
+        onChange={handleChange}
+      />
+      {isNull && <WarningText>{inputTitle} 항목은 필수 정보입니다</WarningText>}
+    </InputWrapper>
+  );
+};
 
 InputBox.propTypes = {
   placeholder: PropTypes.string,
   inputTitle: PropTypes.string,
-  warning: PropTypes.bool,
-  handleClick: PropTypes.func,
 };
 
 InputBox.defaultProps = {
   placeholder: '',
   inputTitle: '',
-  warning: false,
-  handleClick: () => {},
 };
 
 export default InputBox;
@@ -40,13 +63,13 @@ const Input = styled.input`
   border: ${(props) => (props.warning ? '1px solid #ff2e00' : 'none')};
   border-radius: 8px;
   &:focus {
-    outline: ${(props) => (props.warning ? '1px solid red' : '1px solid #000')};
+    outline: ${(props) => (props.warning ? 'none' : '1px solid #000')};
   }
 `;
 
 const WarningText = styled.p`
   width: 100%;
-  margin-top: 8px;
+  margin: 8px 0 24px 0;
   font-size: 12px;
   color: #ff2e00;
 `;
