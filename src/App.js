f99b7c7db7c392ba50d1ad6b2ae236a5f159ce02
y-solb <ysolb99@gmail.com>
2022-03-10 21:01:43 +0900

    Feat: Add Form Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Form from './components/pages/Form';
 import Home from './components/pages/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Form" element={<Form />} />
     </Routes>
   );
 }
