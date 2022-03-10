c9103025f501117874efaa91d912df42a227b224
holystorySeo <holystory.seo@gmail.com>
2022-03-10 17:00:24 +0900

    make mockData

---
+++ src/data/mockData.js
@@ -0,0 +1,32 @@
+const mockData = [
+  {
+    title: '테스트1',
+    id: 1,
+    contents: [
+      {
+        name: '홍길동',
+        phone: '010-1234-0000',
+        address: '서울시 강남구 서초동',
+        gender: '남성',
+        attachFile: '',
+        ruleAgree: true,
+      },
+    ],
+  },
+  {
+    title: '테스트2',
+    id: 2,
+    contents: [
+      {
+        name: '김철수',
+        phone: '010-0000-0000',
+        address: '서울시 강남구 자곡동',
+        성별: '여성',
+        attachFile: '',
+        ruleAgree: true,
+      },
+    ],
+  },
+];
+
+export default mockData;
