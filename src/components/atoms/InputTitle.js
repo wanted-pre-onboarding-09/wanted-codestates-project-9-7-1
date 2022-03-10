e68e8e815ea81f7ad806361521b8718a7c3d185a
y-solb <ysolb99@gmail.com>
2022-03-10 21:02:46 +0900

    Feat: Add InputTitle component

---
+++ src/components/atoms/InputTitle.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const InputTitle = ({ title }) => <Title>{title}</Title>;
+
+InputTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default InputTitle;
+
+const Title = styled.p`
+  margin: 3px 0;
+  font-size: 12px;
+  font-weight: 600;
+`;
