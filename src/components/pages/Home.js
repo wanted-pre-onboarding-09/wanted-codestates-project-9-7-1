09170ec813a5991a28c622878e56f14b79a18f81
ysh2987 <ysh2987@naver.com>
2022-03-11 02:55:03 +0900

    Merge

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../Header';
 import FormList from '../formList/FormList';
-import FormField from '../atoms/FormField';
 
 function Home() {
   return (
     <>
       <Header />
-      <FormField />
       <FormList />
     </>
   );
