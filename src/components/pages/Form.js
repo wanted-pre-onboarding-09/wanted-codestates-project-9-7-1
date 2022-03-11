ac1a219e5d7b7303ea6eb0a960cea2eede54502f
y-solb <ysolb99@gmail.com>
2022-03-11 11:46:46 +0900

    Feat: Add disable state

--- src/components/pages/Form.js
+++ src/components/pages/Form.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormTitle from '../atoms/FormTitle';
 import FormFooter from '../molecules/FormFooter';
 import FormContainer from '../organisms/FormContainer';
 
-const Form = () => (
-  <>
-    <FormTitle title="여기는 폼 제목" />
-    <FormContainer />
-    <FormFooter />
-  </>
-);
+const Form = () => {
+  const [isAllChecked, setIsAllChecked] = useState(false);
+
+  const handleAllCheck = () => {
+    setIsAllChecked(false);
+  };
+
+  return (
+    <>
+      <FormTitle title="여기는 폼 제목" />
+      <FormContainer handleAllCheck={handleAllCheck} />
+      <FormFooter disabled={!isAllChecked} />
+    </>
+  );
+};
 
 export default Form;
