f6319570504ba7e0f0269d61aaa4ca92babfca1d
ysh2987 <ysh2987@naver.com>
2022-03-14 11:32:25 +0900

    deploy

--- src/store/surveyDataSlice.js
+++ src/store/surveyDataSlice.js
@@ -3,51 +3,73 @@ import { createSlice } from '@reduxjs/toolkit';
 // import mock from '../data/mock';
 
 const initialState = {
-  // error: true,
-  // data: mock,
-
-  title: '',
-  formId: 1,
+  maxID: 1,
   data: [
     {
-      id: 'name',
-      type: 'text',
-      required: true,
-      label: '이름',
-      placeholder: '주민등록상 이름 입력',
-    },
-    {
-      id: 'phone',
-      type: 'phone',
-      required: true,
-      label: '휴대폰 번호',
-    },
-    {
-      id: 'address',
-      type: 'address',
-      required: true,
-      label: '배송지',
-    },
-    {
-      id: 'input_0',
-      type: 'select',
-      label: '옵션1',
-      options: ['S', 'L', 'XL', 'XXL'],
-      required: true,
-    },
-    {
-      id: 'input_1',
-      type: 'file',
-      label: '첨부파일',
-      required: false,
-      description: '<p>첨부파일은 위와 같이 입력할 수 있습니다.</p>',
-    },
-    {
-      id: 'agreement_0',
-      type: 'agreement',
-      label: '개인정보 수집 약관 동의',
-      required: true,
-      contents: '<p>(개인정보 수집 및 약관 내용)</p>',
+      title: '테스트1',
+      formId: 1,
+      formData: [
+        {
+          id: 'name',
+          type: 'text',
+          required: true,
+          label: '이름',
+          placeholder: '주민등록상 이름 입력',
+        },
+        {
+          id: 'phone',
+          type: 'phone',
+          required: true,
+          label: '휴대폰 번호',
+        },
+        {
+          id: 'address',
+          type: 'address',
+          required: true,
+          label: '배송지',
+        },
+        {
+          id: 'input_0',
+          type: 'select',
+          label: '옵션1',
+          options: ['남성', '여성'],
+          required: true,
+        },
+        {
+          id: 'input_1',
+          type: 'file',
+          label: '첨부파일',
+          required: false,
+          description: '<p>첨부파일은 위와 같이 입력할 수 있습니다.</p>',
+        },
+        {
+          id: 'agreement_0',
+          type: 'agreement',
+          label: '개인정보 수집 약관 동의',
+          required: true,
+          contents: '<p>(개인정보 수집 및 약관 내용)</p>',
+        },
+      ],
+      resultData: [
+        {
+          useId: 1,
+          name: '홍길동',
+          phone: '010-1234-0000',
+          address: '서울시 강남구 서초동',
+          input_0: '남성',
+          input_1: '',
+          agreement_0: true,
+        },
+        {
+          useId: 2,
+          name: '박동길',
+          phone: '010-2423-0000',
+          address: '서울시 성북구 길음동',
+          input_0: '남성',
+          input_1: '',
+          agreement_0: true,
+        },
+      ],
     },
   ],
 };
@@ -56,11 +78,16 @@ const surveyDataSlice = createSlice({
   name: 'formData',
   initialState,
   reducers: {
+    deleteForm(state, action) {
+      console.log(state);
+      state.data = [state.data.filter((el) => el.formId !== +action.payload)];
+    },
     updateTitle: (state, action) => {
       state.title = action.payload;
     },
 
     updateFormList: (state, action) => {
+      console.log(action.payload);
       state.data = [...action.payload];
     },
 
@@ -210,10 +237,6 @@ const surveyDataSlice = createSlice({
     removeFormList(state, action) {
       const dataset = state.data.filter((form, idx) => idx !== action.payload);
       state.data = dataset;
-    },
-
-    deleteForm(state, action) {
-      state.data = state.data.filter((el) => el.formId !== +action.payload);
     },
     addResultData(state, action) {
       const selectedData = state.data.find(
